fecc1ce9c10e772856b24aee0f40898bfba2bde1
Tahirasif31 <Tahirasif31@gmail.com>
2024-09-04 17:37:16 +0500

    chat added

--- app/page.js
+++ app/page.js
@@ -16,6 +16,15 @@ export default function Home() {
     setPopupOpen(false);
   };
 
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://embed.tawk.to/66d8529450c10f7a00a3eac5/1i6ug5690";
+    script.async = true;
+    script.charset = "UTF-8";
+    script.setAttribute("crossorigin", "*");
+    document.body.appendChild(script);
+  }, []);
+
   // Automatically close the popup after 5 seconds (optional)
   useEffect(() => {
     const timer = setTimeout(() => {
